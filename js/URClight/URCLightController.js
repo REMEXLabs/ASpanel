39a36d5bc59cb1c00f42ded54abf01a317762114
cstrobbe <c_strobbe-github@yahoo.co.uk>
2018-04-27 13:47:37 +0200

    Copyright date & licence URL updated.

--- js/URClight/URCLightController.js
+++ js/URClight/URCLightController.js
@@ -1,17 +1,32 @@
 /*
- * URCLightController class
- * 
- * this class defines the sign-language and urc controller
- * it does:
- * - check if a UIPanel exists
- * - if so, get the settings
- * - check if there exist markers for sign-language content
- * - query the urc-light servers to receive appropriate content
- * - insert the content and set the javascript handlers to display the content
- * 
- * author: Florian Roeser
- * created: June 2, 2013
- */
+ Copyright 2015–2018 Hochschule der Medien (HdM) / Stuttgart Media University
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+  https://github.com/REMEXLabs/ASpanel/blob/master/License.txt
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+/*
+  URCLightController class
+
+  This class defines the sign-language and URC controller.
+  It does:
+  - check if a UIPanel exists
+  - if so, get the settings
+  - check if there exist markers for sign-language content
+  - query the urc-light servers to receive appropriate content
+  - insert the content and set the javascript handlers to display the content
+
+  Author: Florian Roeser
+  Created: 2 June 2013
+*/
+
 
 
 /*
